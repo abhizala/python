23cf7de9fb74a6c5d6e8c66e5b96f73ce6d901cd
kshmr_in <abhizala@hotmail.com>
2019-03-22 19:52:09 +0530

    Colt
    Exercises of the course

---
+++ colt/exam.py
@@ -0,0 +1,2 @@
+# Hello my name is Abhishek
+print("Hello Abhi")
---
+++ colt/mileage.py
@@ -0,0 +1,7 @@
+print("How many kilometers?")
+kms = input()
+miles = float(kms)/1.60934
+miles = round(miles, 2)
+print(f"That is {miles} miles ") 
+
+# round(thing to round, how many decimal points)
